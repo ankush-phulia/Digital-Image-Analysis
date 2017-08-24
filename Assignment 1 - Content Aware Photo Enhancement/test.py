1eff4b4f59ebb20fdd1fbd23c515332234857fdf
ankush-phulia <ankush.phulia22@gmail.com>
2017-08-24 12:06:45 +0530

    Filter, usable image added

--- Assignment 1 - Content Aware Photo Enhancement/test.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-
-src = cv.imread('cow.jpg')
-smooth = cv.edgePreservingFilter(src, flags=1, sigma_s=60, sigma_r=0.4)
-details = src - smooth
-cv.imshow('smooth', smooth)
-cv.imshow('details', details)
-cv.waitKey(0)
